10f8532de221e988a6bad424cfe7fd660bebba80
BugWorm91 <67853859+BugWorm91@users.noreply.github.com>
2021-01-12 23:01:21 +0900

    Delete common.js

--- js/common.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$("#site_header").load("https://bugworm91.github.io/parts/header.html");
-	$("#site_footer").load("https://bugworm91.github.io/parts/footer.html");
-});
-
-$(document).ready(function(){
-	$('#wrapper').css('display', 'block');
-});
