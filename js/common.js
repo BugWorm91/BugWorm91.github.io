fa33e45a1b0f1b7f8601bd6809fe57f03b797fed
BugWorm91 <67853859+BugWorm91@users.noreply.github.com>
2021-01-12 20:59:24 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
 $(function(){
-	$("#site_header").load("https://bugworm91.github.io/parts/site_header.html");
-	$("#site_footer").load("https://bugworm91.github.io/parts/site_footer.html");
+	$("#site_header").load("https://bugworm91.github.io/parts/header.html");
+	$("#site_footer").load("https://bugworm91.github.io/parts/footer.html");
 });
