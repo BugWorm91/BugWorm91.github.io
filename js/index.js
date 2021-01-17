4146ff21d5803fd0f0b7dc6e7cf2d389e38e1162
BugWorm91 <67853859+BugWorm91@users.noreply.github.com>
2021-01-17 17:31:07 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $(function(){
 	});
 	$("#side_contents").load("https://bugworm91.github.io/parts/frame_plain.html", function() {
 		$("#mobile_navigation").load("https://bugworm91.github.io/parts/mobile_menu.html");
-		$("#sub_navigation").load("https://bugworm91.github.io/parts/sub.html");
+		$("#sub_navigation").load("https://bugworm91.github.io/parts/sub_menu.html");
 		$("#news").load("https://bugworm91.github.io/parts/news.html");
 	});
 	$("#site_footer").load("https://bugworm91.github.io/parts/footer.html");
