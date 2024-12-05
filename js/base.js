67caf95aad34d74c1caa3c2842fd4fa2a2b4cd86
BugWorm91 <67853859+BugWorm91@users.noreply.github.com>
2024-12-05 17:32:30 +0900

    Delete js directory

--- js/base.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', function(){
-    let hierarchyStr = createPathHierarchy();
-    fetchInnerHTML(hierarchyStr + 'parts/header.txt', 'site_header');
-    fetchInnerHTML(hierarchyStr + 'parts/footer.txt', 'site_footer');
-});
