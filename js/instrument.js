7cd5538c64cc1493a2045660f897af31a4c9795d
BugWorm91 <67853859+BugWorm91@users.noreply.github.com>
2025-01-24 10:44:49 +0900

    Add files via upload

--- js/instrument.js
+++ js/instrument.js
@@ -1,7 +1,8 @@
-window.addEventListener('DOMContentLoaded', function(){
+function setInstrument(){
     const weeks = ["日", "月", "火", "水", "木", "金", "土"];
     const zodiacs = ["申","酉","戌","亥","子","丑","寅","卯","辰","巳","午","未"];
-    setInterval(function() {
+    
+    setInterval(function(){
         let today = new Date();
         let utcYear = today.getUTCFullYear();
         let year = today.getFullYear();
@@ -27,23 +28,24 @@ window.addEventListener('DOMContentLoaded', function(){
         {
             isLeapYear = "&#x2610";
         }
-
+    
         let lastOfMonth = new Date();
         lastOfMonth.setMonth(lastOfMonth.getMonth() + 1, 0);
-
+    
         let zodiac = zodiacs[year % 12];
-
+    
         let yearEnd = new Date(year, 11, 31);
         let days = Math.ceil((Date.parse(yearEnd) - today.getTime()) / (24 * 60 * 60 * 1000));
-
+    
         document.getElementById("today").innerHTML = "日本標準時(JST)<br>" + year + "/" + month + "/" + date + " " + hours + ":" + minutes + ":" + seconds + "<br>" + week + "曜日";
         document.getElementById("utcToday").innerHTML = "協定世界時(UTC)<br>" + utcYear + "/" + utcMonth + "/" + utcDate + " " + utcHours + ":" + utcMinutes + ":" + utcSeconds + "<br>" + utcWeek + "曜日";
-
+    
         document.getElementById("era").innerHTML = "令和" + era + "年";
         document.getElementById("lastOfMonth").innerHTML = "月末" + lastOfMonth.getDate() + "日<br>" + weeks[lastOfMonth.getDay()] + "曜日";
         document.getElementById("leapYear").innerHTML = "うるう年<br>" + isLeapYear;
         document.getElementById("zodiac").innerHTML = zodiac + "年";
         document.getElementById("days").innerHTML = "今年残り<br>" + days + "日";
     }, 1000);        
-});
+}
+
 
