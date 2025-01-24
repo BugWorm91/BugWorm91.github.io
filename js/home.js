0081bdb85f33213eafcbdee2bbb6893a8ec718e6
BugWorm91 <67853859+BugWorm91@users.noreply.github.com>
2025-01-24 17:07:07 +0900

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
       setInstrument();
+      setDialog('prefectures');
+      setDialog('numbersUnit');
+      setDialog('dataUnit');
+      setDialog('tax');
+      setDialog('symbol');
 });
